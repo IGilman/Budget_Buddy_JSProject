048dc34989acf4e6d79360cdc7976e643661869d
Joshua.Jerome.Hedges <joshua.jerome.hedges@gmail.com>
2020-05-08 14:28:39 -0400

    josh

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let entertainmentInput = document.querySelector(".form");
-let entertainmentTotal = document.querySelector(".total");
+let entertainmentTotal = document.querySelector(".entertainment-sub");
+let foodTotal = document.querySelector(".food-sub");
+let clothingTotal = document.querySelector(".clothing-sub");
+let billsTotal = document.querySelector(".bills-sub");
+let entertainmentInput = document.querySelector(".entertainment-input");
+let foodInput = document.querySelector(".food-input");
+let clothingInput = document.querySelector(".clothing-input");
+let billsInput = document.querySelector(".bills-input");
+
 let budgetTotal = document.querySelector(".budget-form");
 let budgetDisplay = document.querySelector(".budget");
-//const entertainmentObject = [];
+
 let budget = 0;
 let entertainmentSubTotal = 0;
 
 budgetTotal.addEventListener("submit", (event) => {
   event.preventDefault();
   let budgetData = new FormData(budgetTotal);
-  budget = budgetData.get("budget");
-  console.log(budget);
-  budgetDisplay.innerText = `$${budget}`;
+  let budgetAmount = budgetData.get("budget");
+  budget += Number(budgetAmount);
+
+  budgetDisplay.innerText = `$${Number(budget).toFixed(2)}`;
+  budgetTotal.reset();
 });
 
 entertainmentInput.addEventListener("submit", (event) => {
@@ -19,23 +28,12 @@ entertainmentInput.addEventListener("submit", (event) => {
   let entertainmentData = new FormData(entertainmentInput);
   let entertainmentPurchase = entertainmentData.get("item");
   let entertainmentAmount = entertainmentData.get("money-spent");
-  // let counter = 0;
-  // counter += entertainmentAmount;
+
   budget -= entertainmentAmount;
   budgetDisplay.innerText = budget;
   entertainmentSubTotal += Number(entertainmentAmount);
 
-  // entertainmentObject.push({
-  //   purchase: entertainmentPurchase,
-  //   amount: Number(entertainmentAmount),
-  // });
+  entertainmentTotal.innerText = `$${entertainmentSubTotal.toFixed(2)}`;
 
-  // entertainmentObject.forEach((object) => {
-  //   counter += object.amount
-  // });
-
-  entertainmentTotal.innerText = `$${entertainmentSubTotal}.00`;
-  console.log(entertainmentObject);
   entertainmentInput.reset();
 });
-
